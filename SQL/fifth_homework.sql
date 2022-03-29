82b3eaf16141c80694e6238b5b9c031785effdc5
Aysegul Karademir <71903242+aysegulkarademir@users.noreply.github.com>
2022-03-29 23:55:37 +0300

    Create fifth_homework.sql

---
+++ SQL/fifth_homework.sql
@@ -0,0 +1,23 @@
+--1.
+
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length DESC
+LIMIT 5;
+
+
+--2.
+
+SELECT * FROM film
+WHERE title LIKE '%n'
+ORDER BY length ASC
+LIMIT 5
+OFFSET 5;
+
+
+--3.
+
+SELECT * FROM customer
+WHERE store_id = 1
+ORDER BY last_name ASC
+LIMIT 4;
